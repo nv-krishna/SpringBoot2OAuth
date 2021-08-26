dd03ebf492f18c1ac45739a2aff6b5670afcd515
Venkata Krishna <venkata.logtokris@gmail.com>
2021-08-26 22:29:42 +0530

    Spring boot OAuth2 Example

---
+++ src/test/java/com/dev/springboot/oauth/SpringBoot2OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.springboot.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
